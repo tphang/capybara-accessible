fff9ba30f703c10a09f41dde3b02dd4de33868a6
Nishit Shah and Nora Lin <pair+nishit+nora@pivotallabs.com>
2013-07-22 13:42:31 -0400

    Fix running audits against alerts Running audits removes the alert which is unexpected behavior

--- lib/capybara/accessible/element.rb
+++ lib/capybara/accessible/element.rb
@@ -6,11 +6,12 @@ module Capybara
       def click
         synchronize { base.click }
         begin
+          @session.driver.browser.switch_to.alert
+          puts "Skipping accessibility audit: Modal dialog present"
+        rescue ::Selenium::WebDriver::Error::NoAlertOpenError
           if Capybara.current_driver == :accessible && audit_failures.any?
             raise Capybara::Accessible::InaccessibleError, failure_messages
           end
-        rescue ::Selenium::WebDriver::Error::UnhandledAlertError => e
-          puts "Skipping accessibility audit: #{e}"
         end
       end
     end
